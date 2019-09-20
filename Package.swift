5b0aeaacde95a9bc6c4397b5d96623dcca8f1f59
Brian Drelling <brian.drelling@gmail.com>
2019-09-20 13:32:23 -0500

    Migrate build tools to Mint

--- Package.swift
+++ Package.swift
@@ -8,9 +8,6 @@ let package = Package(
         .library(name: "UtilityBelt", targets: ["UtilityBelt"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.40.10"),
-        .package(url: "https://github.com/Realm/SwiftLint", from: "0.33.1"),
-        .package(url: "https://github.com/spothero/Zinc", from: "0.5.0"),
     ],
     targets: [
         .target(
