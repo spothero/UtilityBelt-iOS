fc7ee3407451fe3c85eb5cb23b30b628916dc044
Brian Drelling <brian.drelling@gmail.com>
2019-09-22 22:05:55 -0500

    Created UtilityBeltNetworking library, added HTTPMethod and URL+ExpressibleByStringLiteral

---
+++ Sources/UtilityBeltNetworking/Enums/HTTPMethod.swift
@@ -0,0 +1,16 @@
+// Copyright © 2019 SpotHero, Inc. All rights reserved.
+
+/// HTTP Methods are a set of request methods to indicate the desired action to be performed for a given resource.
+///
+/// Sources:
+/// - [Mozilla - HTTP Methods](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods)
+public enum HTTPMethod: String, Equatable, Hashable {
+    case connect = "CONNECT"
+    case delete = "DELETE"
+    case get = "GET"
+    case head = "HEAD"
+    case options = "OPTIONS"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
