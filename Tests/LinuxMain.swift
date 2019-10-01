7e2a618ec59213eb5baf8ab906eed80a56480199
Brian Drelling <brian.drelling@gmail.com>
2019-10-01 00:43:55 -0500

    Added Sham tests back into LinuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,9 +2,11 @@
 
 import XCTest
 
+import ShamTests
 import UtilityBeltNetworkingTests
 
 var tests = [XCTestCaseEntry]()
+tests += ShamTests.__allTests()
 tests += UtilityBeltNetworkingTests.__allTests()
 
 XCTMain(tests)
