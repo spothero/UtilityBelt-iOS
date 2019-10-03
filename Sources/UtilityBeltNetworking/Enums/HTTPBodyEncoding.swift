5699c4fd794fd6f57d5841b9ab7ce7cd65b05319
Brian Drelling <bdrelling@users.noreply.github.com>
2019-10-03 15:13:04 -0500

    Adjusted comment for the MIME types

--- Sources/UtilityBeltNetworking/Enums/HTTPBodyEncoding.swift
+++ Sources/UtilityBeltNetworking/Enums/HTTPBodyEncoding.swift
@@ -2,8 +2,8 @@
 
 /// Determines the method by which parameters are encoded when sent via HTTP bodies.
 public enum HTTPBodyEncoding {
-    /// Encodes the HTTP body as the application/json.
+    /// Encodes the HTTP body as application/json.
     case json
-    /// Encodes the HTTP body as the application/x-www-form-urlencoded MIME type.
+    /// Encodes the HTTP body as application/x-www-form-urlencoded.
     case wwwFormURLEncoded
 }
