9639ad544f932277643c4338f7814f3139fd9484
Brian Drelling <bdrelling@users.noreply.github.com>
2019-10-04 14:52:39 -0500

    IOS-1341: Implemented URLConvertible and URLRequestConvertible to simplify API (#11)

---
+++ Sources/UtilityBeltNetworking/Enums/UBNetworkError.swift
@@ -0,0 +1,19 @@
+// Copyright © 2019 SpotHero, Inc. All rights reserved.
+
+import Foundation
+
+public enum UBNetworkError: Error {
+    case invalidFilePath(String)
+    case invalidURLString(String)
+}
+
+extension UBNetworkError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case let .invalidFilePath(path):
+            return "Invalid file path: '\(path)'."
+        case let .invalidURLString(urlString):
+            return "Invalid URL string: '\(urlString)'."
+        }
+    }
+}
---
+++ Sources/UtilityBeltNetworking/Protocols/URLRequestConvertible.swift
@@ -0,0 +1,29 @@
+// Copyright © 2019 SpotHero, Inc. All rights reserved.
+
+import Foundation
+
+public protocol URLRequestConvertible {
+    func asURLRequest() throws -> URLRequest
+}
+
+extension URLRequest: URLRequestConvertible {
+    public func asURLRequest() throws -> URLRequest {
+        return self
+    }
+}
+
+extension URL: URLRequestConvertible {
+    public func asURLRequest() throws -> URLRequest {
+        return URLRequest(url: self)
+    }
+}
+
+extension String: URLRequestConvertible {
+    public func asURLRequest() throws -> URLRequest {
+        if let url = URL(string: self) {
+            return URLRequest(url: url)
+        } else {
+            throw UBNetworkError.invalidURLString(self)
+        }
+    }
+}
