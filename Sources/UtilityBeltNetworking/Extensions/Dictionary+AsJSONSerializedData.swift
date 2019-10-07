16dddc2c5005f1cf78a5aa3c1459de5541bc461a
Brian Drelling <brian.drelling@gmail.com>
2019-10-07 16:22:49 -0500

    Added iOS 11.0 restriction to Dictionary+AsJSONSerializedDatA

--- Sources/UtilityBeltNetworking/Extensions/Dictionary+AsJSONSerializedData.swift
+++ Sources/UtilityBeltNetworking/Extensions/Dictionary+AsJSONSerializedData.swift
@@ -4,7 +4,7 @@ import Foundation
 
 extension Dictionary {
     func asJSONSerializedData() throws -> Data {
-        if #available(OSX 10.13, *) {
+        if #available(iOS 11.0, OSX 10.13, *) {
             return try JSONSerialization.data(withJSONObject: self, options: .sortedKeys)
         } else {
             return try JSONSerialization.data(withJSONObject: self, options: [])
