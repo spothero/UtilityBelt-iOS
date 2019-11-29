338590ba5678b3f43177c4890dd094ba6514bfac
Brian Drelling <brian.drelling@gmail.com>
2019-11-29 17:29:19 -0600

    Added demo project and DataStorage target

---
+++ Tests/UtilityBeltDataStorageTests/GeneralTests.swift
@@ -0,0 +1,10 @@
+// Copyright © 2019 SpotHero, Inc. All rights reserved.
+
+@testable import UtilityBeltNetworking
+
+import XCTest
+
+final class GeneralTests: XCTestCase {
+    func testGeneral() {
+    }
+}
---
+++ UtilityBeltTests/UtilityBeltDemo/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UtilityBeltDemo
+//
+//  Created by Brian Drelling on 11/29/19.
+//  Copyright © 2019 SpotHero, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
