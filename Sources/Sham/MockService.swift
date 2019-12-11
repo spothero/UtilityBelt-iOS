326ee4bf7dea2b39009567979d1f10deb9661318
Brian Drelling <brian.drelling@gmail.com>
2019-12-11 11:25:54 -0600

    Fixed OSLog import

--- Sources/Sham/MockService.swift
+++ Sources/Sham/MockService.swift
@@ -1,7 +1,7 @@
 // Copyright © 2019 SpotHero, Inc. All rights reserved.
 
 import Foundation
-import OSLog
+import os
 import UtilityBeltNetworking
 
 /// A service which contains stub requests and stub responses for use with the MockURLProtocol.
