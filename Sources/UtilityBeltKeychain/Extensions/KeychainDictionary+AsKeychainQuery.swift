a4f7e209cade5782baad828bffc82a1902032d6c
Brian Drelling <brian.drelling@gmail.com>
2019-12-17 15:25:39 -0600

    Fixed comment

--- Sources/UtilityBeltKeychain/Extensions/KeychainDictionary+AsKeychainQuery.swift
+++ Sources/UtilityBeltKeychain/Extensions/KeychainDictionary+AsKeychainQuery.swift
@@ -7,7 +7,7 @@ typealias KeychainDictionary = [KeychainAttribute: Any?]
 extension KeychainDictionary {
     /// Converts the dictionary into a Keychain query-compatible CFDictionary.
     func asKeychainQuery() -> CFDictionary {
-        // Reduce the KeychainDictionary into a [String: Any] dictionary,
+        // Reduce the KeychainDictionary into a [AnyHashable: Any] dictionary,
         // converting enum keys into their raw string values and ignoring all nil values.
         let stringKeyedDictionary = self.reduce(into: [:]) { result, currentItem in
             if let value = currentItem.value {
