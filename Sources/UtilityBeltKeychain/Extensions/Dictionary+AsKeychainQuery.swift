1b5db55b31e3830911ea8ea5ae87ab95d711dc99
Brian Drelling <brian.drelling@gmail.com>
2019-12-17 14:31:32 -0600

    IOS-1546: Implemented KeychainQuery extension method

---
+++ Sources/UtilityBeltKeychain/Extensions/Dictionary+AsKeychainQuery.swift
@@ -0,0 +1,23 @@
+// Copyright © 2019 SpotHero, Inc. All rights reserved.
+
+import Foundation
+
+typealias KeychainDictionary = [KeychainAttribute: Any?]
+
+extension Dictionary where Key == KeychainAttribute, Value == Any? {
+    /// Convert the key from enum into raw string values and removes all nil values.
+    func asKeychainQuery() -> CFDictionary {
+        var stringKeyedDictionary = [String: Any]()
+        
+        for (key, value) in self {
+            guard var value = value else {
+                continue
+            }
+
+            stringKeyedDictionary[key.rawValue] = value
+            
+        }
+        
+        return stringKeyedDictionary as CFDictionary
+    }
+}
