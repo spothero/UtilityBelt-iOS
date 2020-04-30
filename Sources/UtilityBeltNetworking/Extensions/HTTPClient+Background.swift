7d7b2157f8ac53a3f164b2657bbadbfd66a70467
Luke <luke@spothero.com>
2020-04-30 15:23:28 -0500

    Unnecessary word

--- Sources/UtilityBeltNetworking/Extensions/HTTPClient+Background.swift
+++ Sources/UtilityBeltNetworking/Extensions/HTTPClient+Background.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 public extension HTTPClient {
-    /// An HTTPClient set up to work with in the background.
+    /// An HTTPClient set up to work in the background.
     /// - Parameter identifier: The identifier for the backgrounded request configuration.
     static func background(withIdentifier identifier: String) -> HTTPClient {
         let sessionDelegate = HTTPBackgroundSessionDelegate()
