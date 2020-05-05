70303b0587ff62668a12a2a097f66976f1da59c2
Luke <lquigley@users.noreply.github.com>
2020-05-05 14:45:49 -0500

    Test parameter serialization. (#65)

--- Tests/UtilityBeltNetworkingTests/Tests/ParameterEncodingTests.swift
+++ Tests/UtilityBeltNetworkingTests/Tests/ParameterEncodingTests.swift
@@ -4,7 +4,8 @@
 import XCTest
 
 final class ParameterEncodingTests: XCTestCase {
-    var parameters: [String: Any] = [
+    
+    private var parameters: [String: Any] = [
         "bool": true,
         "double": 2.5,
         "integer": 1,
@@ -32,20 +33,150 @@ final class ParameterEncodingTests: XCTestCase {
             "carol",
         ],
     ]
-
-    // This is totally not a test case, this is just me hacking around for now -- ignore it
-    func testParameterEncoding() {
-        var components = URLComponents(string: "localhost")
-        components?.setQueryItems(with: self.parameters)
-
-        guard let queryItems = components?.queryItems else {
+    
+    func testPostDefaultRequest() {
+        // GIVEN: I create a POST request.
+        guard let request = try? self.request(withMethod: .post) else {
+            assertionFailure("Failed to create request.")
             return
         }
-
-        for item in queryItems {
-            print(item.name, String(describing: item.value))
-        }
-
-        print(String(describing: components?.percentEncodedQuery))
+        
+        // THEN: The body contains the serialized data.
+        self.dataIsInBody(request: request)
     }
+    
+    func testPostQueryRequest() {
+        // GIVEN: I create a POST request with query encoding.
+        guard let request = try? self.request(withMethod: .post, encoding: .queryString) else {
+            assertionFailure("Failed to create request.")
+            return
+        }
+        
+        // THEN: The query contains the serialized data.
+        self.queryIsInParams(request: request)
+    }
+    
+    func testPostFormRequest() {
+        // GIVEN: I create a POST request with form encoding.
+        guard let request = try? self.request(withMethod: .post, encoding: .httpBody(.wwwFormURLEncoded)) else {
+            assertionFailure("Failed to create request.")
+            return
+        }
+        
+        // THEN: The body contains the serialized data.
+        self.queryIsInBody(request: request)
+    }
+    
+    func testGetDefaultRequest() {
+        // GIVEN: I create a GET request.
+        guard let request = try? self.request(withMethod: .get) else {
+            assertionFailure("Failed to create request.")
+            return
+        }
+        
+        // THEN: The query contains the serialized data.
+        self.queryIsInParams(request: request)
+    }
+    
+    func testDeleteDefaultRequest() {
+        // GIVEN: I create a DELETE request.
+        guard let request = try? self.request(withMethod: .delete) else {
+            assertionFailure("Failed to create request.")
+            return
+        }
+        
+        // THEN: The query contains the serialized data.
+        self.queryIsInParams(request: request)
+    }
+    
+    func testPutDefaultRequest() {
+        // GIVEN: I create a PUT request.
+        guard let request = try? self.request(withMethod: .put) else {
+            assertionFailure("Failed to create request.")
+            return
+        }
+        
+        // THEN: The body contains the serialized data.
+        self.dataIsInBody(request: request)
+    }
+    
+    func testPatchDefaultRequest() {
+        // GIVEN: I create a PATCH request.
+        guard let request = try? self.request(withMethod: .patch) else {
+            assertionFailure("Failed to create request.")
+            return
+        }
+        
+        // THEN: The body contains the serialized data.
+        self.dataIsInBody(request: request)
+    }
+    
+    /// Helper to create a request.
+    /// - Parameters:
+    ///   - method: The HTTP method for the request.
+    ///   - encoding: The encoding for the parameters
+    /// - Throws: If there's an error creating the request.
+    /// - Returns: The URLRequest
+    private func request(withMethod method: HTTPMethod, encoding: ParameterEncoding? = nil) throws -> URLRequest {
+        let url = try "spothero.com".asURL()
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        request.setParameters(self.parameters, method: method, encoding: encoding)
+        
+        return request
+    }
+    
+    /// Asserts that the parameters are data in the request body.
+    /// - Parameter request: The request to check.
+    private func dataIsInBody(request: URLRequest) {
+        let jsonSerialized: Data
+        if #available(OSX 10.13, *) {
+            guard let serialized = try? JSONSerialization.data(withJSONObject: self.parameters, options: .sortedKeys) else {
+                assertionFailure("Failed to serialized mocked parameters")
+                return
+            }
+            jsonSerialized = serialized
+        } else {
+            guard let serialized = try? JSONSerialization.data(withJSONObject: self.parameters, options: []) else {
+                assertionFailure("Failed to serialized mocked parameters")
+                return
+            }
+            jsonSerialized = serialized
+        }
+        
+        XCTAssertEqual(request.httpBody, jsonSerialized)
+    }
+    
+    /// Asserts that the parameters are a query in the request body.
+    /// - Parameter request: The request to check.
+    private func queryIsInBody(request: URLRequest) {
+        var components = URLComponents()
+        components.setQueryItems(with: self.parameters)
+        guard let testQueryData = components.percentEncodedQuery?.data(using: .utf8) else {
+            assertionFailure("Failed to create test query.")
+            return
+        }
+        
+        XCTAssertEqual(request.httpBody, testQueryData)
+    }
+    
+    /// Asserts that the parameters are a query on the request.
+    /// - Parameter request: The request to check.
+    private func queryIsInParams(request: URLRequest) {
+        guard let requestQuery = request.url?.query else {
+            assertionFailure("Failed to get request query.")
+            return
+        }
+        
+        var components = URLComponents()
+        components.setQueryItems(with: self.parameters)
+        guard let testQuery = components.percentEncodedQuery else {
+            assertionFailure("Failed to create test query.")
+            return
+        }
+        
+        XCTAssertEqual(requestQuery, testQuery, "Query is incorrect.")
+    }
+    
 }
