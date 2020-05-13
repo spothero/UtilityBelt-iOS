a9c227911b21e66b08ca917663ee908cd75b3dff
Brian Drelling <brian.drelling@gmail.com>
2020-05-12 21:16:11 -0500

    Updating the SwiftLint version

--- scripts/danger_lint.sh
+++ scripts/danger_lint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Sets the SWIFTLINT_VERSION environment variable for Danger
-export SWIFTLINT_VERSION=$(mint run swiftlint version)
+# Checks if mint is installed
+if [ -x "$(command -v mint)" ]; then
+  # If mint is installed, set Danger's SwiftLint version to the same version in our Mintfile
+    export SWIFTLINT_VERSION=$(mint run swiftlint version)
+fi
 
 # Runs Danger
-bundle exec danger --dangerfile=Dangerfile-Lint --fail-on-errors=true --remove-previous-comments --new-comment --verbose
+bundle exec danger dry_run --dangerfile=Dangerfile-Lint --fail-on-errors=true --remove-previous-comments --new-comment --verbose
