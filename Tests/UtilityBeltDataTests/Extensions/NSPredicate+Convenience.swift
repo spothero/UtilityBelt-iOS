6b66121fbc19fc5eb6746eae5ece098b58dc0358
Kyle Haptonstall <kyle.haptonstall@spothero.com>
2020-06-15 16:33:43 -0500

    added predicate convenience extension

---
+++ Tests/UtilityBeltDataTests/Extensions/NSPredicate+Convenience.swift
@@ -0,0 +1,17 @@
+// Copyright © 2020 SpotHero, Inc. All rights reserved.
+
+import Foundation
+
+extension NSPredicate {
+    convenience init(key: String, equalTo value: Any) {
+        self.init(key, "==", value)
+    }
+    
+    convenience init(key: String, contains value: String) {
+        self.init(key, "CONTAINS[cd]", value)
+    }
+    
+    private convenience init(_ key: String, _ operatorFunc: String, _ value: Any) {
+        self.init(format: "%K \(operatorFunc) %@", argumentArray: [key, value])
+    }
+}
