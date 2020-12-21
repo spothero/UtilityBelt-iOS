7407288f6d24a9b7db1938790d86e5f9df7ae5f5
Luke Quigley <lquigley@users.noreply.github.com>
2020-12-21 15:16:12 -0600

    Update Sources/Sham/Protocols/LaunchEnvironmentObject.swift
    Co-authored-by: Kyle Haptonstall <khaptonstall@users.noreply.github.com>

--- Sources/Sham/Protocols/LaunchEnvironmentObject.swift
+++ Sources/Sham/Protocols/LaunchEnvironmentObject.swift
@@ -6,6 +6,7 @@ import Foundation
 public protocol LaunchEnvironmentObject {
     /// The key used when saving and retrieving the encoded object in the launch environment.
     static var launchEnvironmentKey: String { get }
+    
     /// The object encoded into a string value to be set in the launch environment.
     func encodedAsString() throws -> String?
 }
