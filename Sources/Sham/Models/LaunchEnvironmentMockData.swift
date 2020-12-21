908abdfa93eccec39c6a9064bce31c8d4c83ee26
Luke Quigley <lquigley@users.noreply.github.com>
2020-12-21 15:25:01 -0600

    Update Sources/Sham/Models/LaunchEnvironmentMockData.swift
    Co-authored-by: Kyle Haptonstall <khaptonstall@users.noreply.github.com>

--- Sources/Sham/Models/LaunchEnvironmentMockData.swift
+++ Sources/Sham/Models/LaunchEnvironmentMockData.swift
@@ -36,6 +36,7 @@ public final class LaunchEnvironmentMockData: Codable {
 extension LaunchEnvironmentMockData: LaunchEnvironmentEncodable {
     /// The key used to save and retrieve the object in the launch environment.
     public static var launchEnvironmentKey = "mocked-data"
+    
     // The encoded value of the object in String form, throws an error if unable to encode.
     public func encodedAsString() throws -> String? {
         let encodedValue = try JSONEncoder().encode(self)
