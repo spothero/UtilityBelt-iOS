127acfc60ee17ac8061aae67a23a24694d48dcc4
Luke Quigley <lquigley@users.noreply.github.com>
2020-12-22 09:50:11 -0600

    Update Sources/Sham/Extensions/StubbedDataCollection+LaunchEnvironmentEncodable.swift
    Co-authored-by: Kyle Haptonstall <khaptonstall@users.noreply.github.com>

--- Sources/Sham/Extensions/StubbedDataCollection+LaunchEnvironmentEncodable.swift
+++ Sources/Sham/Extensions/StubbedDataCollection+LaunchEnvironmentEncodable.swift
@@ -5,6 +5,7 @@ import Foundation
 extension StubbedDataCollection: LaunchEnvironmentEncodable {
     /// The key used to save and retrieve the object in the launch environment.
     public static var launchEnvironmentKey = "mocked-data"
+    
     // The encoded value of the object in String form, throws an error if unable to encode.
     public func encodedAsString() throws -> String? {
         let encodedValue = try JSONEncoder().encode(self)
