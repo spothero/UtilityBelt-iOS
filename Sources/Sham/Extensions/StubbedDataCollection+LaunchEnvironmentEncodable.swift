b698a112b6c532c99a29656f66b779f4049a26e8
Luke Quigley <luke@spothero.com>
2020-12-22 10:38:33 -0600

    Use built-in doc helper

--- Sources/Sham/Extensions/StubbedDataCollection+LaunchEnvironmentEncodable.swift
+++ Sources/Sham/Extensions/StubbedDataCollection+LaunchEnvironmentEncodable.swift
@@ -6,7 +6,9 @@ extension StubbedDataCollection: LaunchEnvironmentEncodable {
     /// The key used to save and retrieve the object in the launch environment.
     public static var launchEnvironmentKey = "mocked-data"
     
-    // The encoded value of the object in String form, throws an error if unable to encode.
+    /// The encoded value of the object in String form.
+    /// - Throws: Throws an error if unable to encode.
+    /// - Returns: The encoded value of the object in String form.
     public func encodedAsString() throws -> String? {
         let encodedValue = try JSONEncoder().encode(self)
         return String(data: encodedValue, encoding: .utf8)
