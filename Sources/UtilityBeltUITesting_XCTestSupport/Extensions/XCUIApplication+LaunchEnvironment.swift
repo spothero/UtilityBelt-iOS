b4263fd479c04cf0e21903d30fee9d72f38820d4
Luke Quigley <luke@spothero.com>
2021-01-06 15:44:06 -0600

    LaunchEnvironmentEncodable -> LaunchEnvironmentCodable

--- Sources/UtilityBeltUITesting_XCTestSupport/Extensions/XCUIApplication+LaunchEnvironment.swift
+++ Sources/UtilityBeltUITesting_XCTestSupport/Extensions/XCUIApplication+LaunchEnvironment.swift
@@ -1,4 +1,4 @@
-// Copyright © 2020 SpotHero, Inc. All rights reserved.
+// Copyright © 2021 SpotHero, Inc. All rights reserved.
 
 #if canImport(XCTest)
 
@@ -9,7 +9,7 @@
         /// Convenience method for setting up the launch environment with specific information and launching the application.
         /// - Parameter objects: An array of launch environment encodable objects that will be encoded into the launch environment.
         /// - Throws: Will throw an error if an object is unable to be encoded into the launch environment.
-        func launch(withEnvironmentObjects objects: [LaunchEnvironmentEncodable]) throws {
+        func launch(withEnvironmentObjects objects: [LaunchEnvironmentCodable]) throws {
             try objects.forEach { object in
                 let environmentKey = type(of: object).launchEnvironmentKey
                 // TODO: https://spothero.atlassian.net/browse/IOS-2690
