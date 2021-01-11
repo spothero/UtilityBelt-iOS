aa2c268159e6199c1e0f717008e1ba37c44eda85
Kyle Haptonstall <kyle.haptonstall@spothero.com>
2021-01-11 11:53:02 -0600

    Added manual Codable conformance

--- Sources/UtilityBeltUITesting/Models/UserDefaultsConfiguration.swift
+++ Sources/UtilityBeltUITesting/Models/UserDefaultsConfiguration.swift
@@ -3,7 +3,16 @@
 import Foundation
 
 public final class UserDefaultsConfiguration: LaunchEnvironmentCodable {
+    // MARK: CodingKeys
+    
+    private enum CodingKeys: CodingKey {
+        case standardDictionaryData
+        case customSuitesDictionaryData
+    }
+    
     // MARK: Properties
+    
+    public static let launchEnvironmentKey = "user-defaults-configuration"
     
     /// A dictionary represent key/value pairs to be written to the `.standard` UserDefaults.
     public private(set) var standardDictionary: [String: Any] = [:]
@@ -26,5 +35,35 @@ public final class UserDefaultsConfiguration: LaunchEnvironmentCodable {
         }
         
         self.customSuitesDictionary[suite]?[key] = value
+    }
+   
+    // MARK: Decodable
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let standardDictionaryData = try container.decode(Data.self,
+                                                          forKey: .standardDictionaryData)
+        self.standardDictionary = try JSONSerialization
+            .jsonObject(with: standardDictionaryData, options: []) as? [String: Any] ?? [:]
+        
+        let customSuitesDictionaryData = try container.decode(Data.self,
+                                                              forKey: .customSuitesDictionaryData)
+        self.customSuitesDictionary = try JSONSerialization
+            .jsonObject(with: customSuitesDictionaryData, options: []) as? [String: [String: Any]] ?? [:]
+    }
+    
+    // MARK: Encodable
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        let standardDictionaryData = try JSONSerialization.data(withJSONObject: self.standardDictionary,
+                                                                options: [])
+        try container.encode(standardDictionaryData, forKey: .standardDictionaryData)
+        
+        let customSuitesDictionaryData = try JSONSerialization.data(withJSONObject: self.customSuitesDictionary,
+                                                                    options: [])
+        try container.encode(customSuitesDictionaryData, forKey: .customSuitesDictionaryData)
     }
 }
