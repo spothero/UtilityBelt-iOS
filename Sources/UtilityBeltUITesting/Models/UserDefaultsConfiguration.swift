e6e11f15dd6f0f6f77e59953c6a2d61ec8ea601c
Kyle Haptonstall <kyle.haptonstall@spothero.com>
2021-01-11 17:56:29 -0600

    Updated for synthesized Codable conformance using UserDefaultsObject

--- Sources/UtilityBeltUITesting/Models/UserDefaultsConfiguration.swift
+++ Sources/UtilityBeltUITesting/Models/UserDefaultsConfiguration.swift
@@ -3,79 +3,37 @@
 import Foundation
 
 public final class UserDefaultsConfiguration: LaunchEnvironmentCodable {
-    // MARK: CodingKeys
-    
-    private enum CodingKeys: CodingKey {
-        case standardDictionaryData
-        case customSuitesDictionaryData
-    }
-    
     // MARK: Properties
     
     public static let launchEnvironmentKey = "user-defaults-configuration"
     
-    /// A dictionary represent key/value pairs to be written to the `.standard` UserDefaults.
-    public private(set) var standardDictionary: [String: Any] = [:]
+    /// An array representing key/value pairs to be written to the `.standard` UserDefaults.
+    public private(set) var standard: [UserDefaultsObject] = []
     
-    /// A dictionary represent key/value pairs to be written to UserDefaults where the
+    /// A dictionary representing key/value pairs to be written to UserDefaults where the
     /// top-level key is the name of the suite, and the value is a dictionary of UserDefaults.
-    public private(set) var customSuitesDictionary: [String: [String: Any]] = [:]
+    public private(set) var customSuites: [String: [UserDefaultsObject]] = [:]
     
     // MARK: Storing UserDefaults
     
-    /// Stores a value for the given key in the standard UserDefaults dictionary.
-    public func storeValue(_ value: Any, forKey key: String) {
-        self.standardDictionary[key] = value
+    /// Stores the `UserDefaultsObject` for use in `UserDefaults.standard`.
+    public func storeUserDefaultsObject(_ object: UserDefaultsObject) {
+        self.standard.removeAll(where: { $0.key == object.key })
+        self.standard.append(object)
     }
-    
-    /// Stores a value for the given key in a dictionary under the given suite name.
-    public func storeValue(_ value: Any, forKey key: String, inSuite suite: String) {
-        if self.customSuitesDictionary[suite] == nil {
-            self.customSuitesDictionary[suite] = [:]
+
+    /// Stores a value `UserDefaultsObject` in an array under the given suite name.
+    public func storeUserDefaultsObject(_ object: UserDefaultsObject, inSuite suite: String) {
+        if self.customSuites[suite] == nil {
+            self.customSuites[suite] = []
         }
-        
-        self.customSuitesDictionary[suite]?[key] = value
+
+        self.customSuites[suite]?.removeAll(where: { $0.key == object.key })
+
+        self.customSuites[suite]?.append(object)
     }
    
-    // MARK: Decodable
+    // MARK: Initialization
     
     public init() {}
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        let standardDictionaryData = try container.decode(Data.self, forKey: .standardDictionaryData)
-        let standardDictionaryJSON = try JSONSerialization.jsonObject(with: standardDictionaryData, options: [])
-        guard let standardDictionary = standardDictionaryJSON as? [String: Any] else {
-            let debugDescription = "Value for .standardDictionaryData could not be cast to [String: Any]."
-            throw DecodingError.dataCorruptedError(forKey: .standardDictionaryData,
-                                                   in: container,
-                                                   debugDescription: debugDescription)
-        }
-        self.standardDictionary = standardDictionary
-        
-        let customSuitesDictionaryData = try container.decode(Data.self, forKey: .customSuitesDictionaryData)
-        let customSuitesDictionaryJSON = try JSONSerialization.jsonObject(with: customSuitesDictionaryData, options: [])
-        guard let customSuitesDictionary = customSuitesDictionaryJSON as? [String: [String: Any]] else {
-            let debugDescription = "Value for .customSuitesDictionaryData could not be cast to [String: [String: Any]]."
-            throw DecodingError.dataCorruptedError(forKey: .customSuitesDictionaryData,
-                                                   in: container,
-                                                   debugDescription: debugDescription)
-        }
-        self.customSuitesDictionary = customSuitesDictionary
-    }
-    
-    // MARK: Encodable
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        let standardDictionaryData = try JSONSerialization.data(withJSONObject: self.standardDictionary,
-                                                                options: [])
-        try container.encode(standardDictionaryData, forKey: .standardDictionaryData)
-        
-        let customSuitesDictionaryData = try JSONSerialization.data(withJSONObject: self.customSuitesDictionary,
-                                                                    options: [])
-        try container.encode(customSuitesDictionaryData, forKey: .customSuitesDictionaryData)
-    }
 }
