ada0f92ed31fc0be291282e7add5f959c75fabfb
Luke Quigley <luke@spothero.com>
2021-01-15 11:13:51 -0600

    Create extension to set and remove launch arguments

---
+++ Sources/UtilityBeltUITesting_XCTestSupport/Extensions/XCUIApplication+LaunchArguments.swift
@@ -0,0 +1,28 @@
+// Copyright © 2021 SpotHero, Inc. All rights reserved.
+
+#if canImport(XCTest)
+
+    import UtilityBeltUITesting
+    import XCTest
+
+    public extension XCUIApplication {
+        /// Convenience method for merging new launch arguments into the launch arguments array.
+        /// - Parameter objects: An array of launch argument objects that will be adding into the launch arguments.
+        func updateLaunchArguments(_ arguments: [LaunchArgument]) {
+            arguments.forEach { argument in
+                // Remove the argument in the case that it already there.
+                self.removeLaunchArgument(argument)
+                self.launchArguments += [argument.rawValue]
+            }
+        }
+        
+        /// Remove an argument from the launch arguments, if it exists.
+        /// - Parameter argument: The argument to remove.
+        func removeLaunchArgument(_ argument: LaunchArgument) {
+            self.launchArguments.removeAll(where: { existingValue in
+                existingValue == argument.rawValue
+            })
+        }
+    }
+
+#endif
