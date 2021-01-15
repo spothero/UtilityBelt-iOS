ea1c8ae24b434b78c286cf7184479ebe45de1ef9
Luke Quigley <luke@spothero.com>
2021-01-15 11:54:49 -0600

    Be more Swifty with launch argument updates

--- Sources/UtilityBeltUITesting_XCTestSupport/Extensions/XCUIApplication+LaunchArguments.swift
+++ Sources/UtilityBeltUITesting_XCTestSupport/Extensions/XCUIApplication+LaunchArguments.swift
@@ -9,19 +9,7 @@
         /// Convenience method for merging new launch arguments into the launch arguments array.
         /// - Parameter objects: An array of launch argument objects that will be adding into the launch arguments.
         func updateLaunchArguments(_ arguments: [LaunchArgument]) {
-            arguments.forEach { argument in
-                // Remove the argument in the case that it already exists.
-                self.removeLaunchArgument(argument)
-                self.launchArguments += [argument.rawValue]
-            }
-        }
-        
-        /// Remove an argument from the launch arguments, if it exists.
-        /// - Parameter argument: The argument to remove.
-        func removeLaunchArgument(_ argument: LaunchArgument) {
-            self.launchArguments.removeAll(where: { existingValue in
-                existingValue == argument.rawValue
-            })
+            self.launchArguments += arguments.map(\.rawValue).filter { !self.launchArguments.contains($0) }
         }
     }
 
