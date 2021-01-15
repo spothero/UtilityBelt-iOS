1bb5a14e6c02d4b25ed9afee22ec50e69a2168ca
Luke Quigley <luke@spothero.com>
2021-01-15 11:13:28 -0600

    Create the launch argument enum

---
+++ Sources/UtilityBeltUITesting/Enumerations/LaunchArgument.swift
@@ -0,0 +1,8 @@
+// Copyright © 2021 SpotHero, Inc. All rights reserved.
+
+import Foundation
+
+/// An enumeration of launch arguments used when launching an XCUIApplication.
+public enum LaunchArgument: String {
+    case xcuiTestCase
+}
