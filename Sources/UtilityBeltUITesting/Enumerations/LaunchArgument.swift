984faa7129815aadf0021151504a9134b3f07e7e
Luke Quigley <lquigley@users.noreply.github.com>
2021-01-19 16:27:18 -0600

    Update Sources/UtilityBeltUITesting/Enumerations/LaunchArgument.swift
    Co-authored-by: Kyle Haptonstall <khaptonstall@users.noreply.github.com>

--- Sources/UtilityBeltUITesting/Enumerations/LaunchArgument.swift
+++ Sources/UtilityBeltUITesting/Enumerations/LaunchArgument.swift
@@ -4,7 +4,7 @@ import Foundation
 
 /// An enumeration of launch arguments used when launching an XCUIApplication.
 public enum LaunchArgument {
-    /// An custom argument.
+    /// A custom argument.
     case custom(value: String)
     // TODO: https://spothero.atlassian.net/browse/IOS-2719 Set up proper command line arguments and user defaults.
 }
