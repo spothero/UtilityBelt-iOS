ecbdd90ccb40b574bb799fd9ae68aa06e518f4b3
Luke Quigley <luke@spothero.com>
2021-02-26 12:31:55 -0600

    Add documentation

--- Sources/UtilityBeltNetworking/Extensions/NSNumber+Boolean.swift
+++ Sources/UtilityBeltNetworking/Extensions/NSNumber+Boolean.swift
@@ -3,6 +3,8 @@
 import Foundation
 
 extension NSNumber {
+    /// Returns wether or not the underlying value is a boolean value.
+    /// Used to distinguish the difference between a zero and false value or a one and true value.
     var isBoolean: Bool {
         return CFGetTypeID(self as CFTypeRef) == CFBooleanGetTypeID()
     }
