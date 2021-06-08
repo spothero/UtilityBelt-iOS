79d69e30d28f29ab7f96692bd641ada3093d9619
Brian Drelling <brian.drelling@gmail.com>
2021-06-08 12:05:05 -0500

    IOS-3062: Removed XCUI Testing code from UtilityBelt

--- Sources/UtilityBeltUITesting/Extensions/StubbedDataCollection+LaunchEnvironmentCodable.swift
+++
@@ -1,9 +0,0 @@
-// Copyright © 2021 SpotHero, Inc. All rights reserved.
-
-import Foundation
-import Sham
-
-extension StubbedDataCollection: LaunchEnvironmentCodable {
-    /// The key used to save and retrieve the object in the launch environment.
-    public static var launchEnvironmentKey = "mocked-data"
-}
