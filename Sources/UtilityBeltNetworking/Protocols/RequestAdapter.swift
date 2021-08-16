9d8dd82c11161564a494302a80b4fb26b44eafbe
Kyle Haptonstall <kyle.haptonstall@spothero.com>
2021-08-16 14:45:12 -0500

    Created a protocol that allows adapting a URLRequest

---
+++ Sources/UtilityBeltNetworking/Protocols/RequestAdapter.swift
@@ -0,0 +1,12 @@
+// Copyright © 2021 SpotHero, Inc. All rights reserved.
+
+import Foundation
+
+public protocol RequestAdapter {
+    /// Allows for adapting a `URLRequest` prior to the request being sent to the server.
+    /// - Parameters:
+    ///   - request: The `URLRequest` to adapt.
+    ///   - completion: A closure that must be executed when request adapting is complete. If you
+    /// do not wish to adapt the request, call the closure with a `.success`, passing in the original request.
+    func adapt(_ request: URLRequest, completion: (Swift.Result<URLRequest, Error>) -> Void)
+}
